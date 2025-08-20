d98e7fe1499cc093f72745ac06e13b04db8b2c97
GrammerFox <johuaferiagiil@gmail.com>
2025-08-19 19:59:43 -0500

    logica 3

---
+++ logica ejerc/index2.js
@@ -0,0 +1,70 @@
+// let rango = parseInt(prompt("ingresa un nu entero"));
+
+// if(rango >= 20 && rango <= 70) {
+//alert("Hace parte del rango");
+// alert("Hace parte del rango");
+
+// } else{
+//alert("No hace parte del rango")
+// }
+
+//let num = parseInt(prompt("ingresa el valor"))
+
+//if(num >= 100 && num <= 999) {
+//  alert("El nuero tienes 3 digitos")
+//} else {
+//  alert("El nuero no tiene 3 digitos")
+//}
+
+//let num = parseInt(prompt("Ingresa el valor"))
+
+//let rest = num % 2;
+//let par = rest == 0;
+
+//console.log("Es par? " + par)
+
+//if (num >= 100 && num <= 999) {
+    //if (num % 2 === 0) {
+        //console.log("EL nuero es par y tiene 3 digitos")
+    //} else {
+       // console.log("El nuero es inpar y tiene 3 digitos")
+  //  }
+//} else {
+    //alert("El nuero no tiene 3 digitos")
+//};
+
+//let nota = prompt("ingresa la nota")
+
+//nota = parseFloat(nota)
+
+//if(nota <= 2){
+   // alert("perdio el periodo");
+//} else if (nota >= 2 && nota <= 3.9){
+   // alert("Paso el periodo arrastrado");
+//} else {
+    //alert("Gano el año perfecto");
+//};
+
+//let salario = 21.000; 
+
+//if(salario < 10.000){
+    //alert("tiene 0%")
+//} else if(salario > 10.000 && salario <= 20.000 ){
+    //alert("tiene el 10%");
+//}else{
+    //alert("tiene el 15%");                                      
+//};
+
+//let año = parseInt(prompt("ingresa el valor"));
+
+//if( año %2 && año/100 != 0){
+    //alert("el año es bisiesto");
+
+//}else {
+   // alert("el año no es biciesto");
+//};
+
+
+
+
+
